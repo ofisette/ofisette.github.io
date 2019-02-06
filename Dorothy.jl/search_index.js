b89e259ea517ee2ab59e19150d5440335e4ecdf0
Olivier Fisette <ofisette@gmail.com>
2019-02-06 15:30:01 +0100

    Dorothy documentation, first deployment

---
+++ Dorothy.jl/search_index.js
@@ -0,0 +1,147 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Dorothy.jl-1",
+    "page": "Home",
+    "title": "Dorothy.jl",
+    "category": "section",
+    "text": "Julia package for molecular structure manipulation and analysis"
+},
+
+{
+    "location": "#License-1",
+    "page": "Home",
+    "title": "License",
+    "category": "section",
+    "text": "You can use Dorothy under the terms of the MIT “Expat” License; see LICENSE.md in the project files, or Software license in the online documentation."
+},
+
+{
+    "location": "#Status-1",
+    "page": "Home",
+    "title": "Status",
+    "category": "section",
+    "text": "Dorothy is in early development, and targets Julia 1.x."
+},
+
+{
+    "location": "#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "Dorothy is not a registered package. You can add it to your Julia environment by giving the URL to its repository. The unregistered packages Formats, FormatCodecs and FormatStreams are required as dependencies and can be installed in the same manner.using Pkg\n\nPkg.add(\"https://github.com/ofisette/Formats.jl\")\nPkg.add(\"https://github.com/ofisette/FormatCodecs.jl\")\nPkg.add(\"https://github.com/ofisette/FormatStreams.jl\")\nPkg.add(\"https://github.com/ofisette/Dorothy.jl\")"
+},
+
+{
+    "location": "#Documentation-1",
+    "page": "Home",
+    "title": "Documentation",
+    "category": "section",
+    "text": "Documentation is available online, and can be built from the project files. Documentation for the the public API is also available directly from the Julia REPL."
+},
+
+{
+    "location": "#Citing-1",
+    "page": "Home",
+    "title": "Citing",
+    "category": "section",
+    "text": "If you use Dorothy in your research, please cite the software in your publications. There is currently no published paper describing Dorothy, but it can still be cited as follows:Olivier Fisette. Dorothy: Julia package for molecular structure manipulation and analysis, version 0.1, 2019. https://github.com/ofisette/Dorothy.jl"
+},
+
+{
+    "location": "#Community-1",
+    "page": "Home",
+    "title": "Community",
+    "category": "section",
+    "text": "Dorothy is developed by Olivier Fisette in the Center for Theoretical Chemistry at Ruhr-University Bochum, Germany.Contributions, bug reports and feature suggestions are welcome. Development is tracked in the project’s repository."
+},
+
+{
+    "location": "intro/#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "intro/#Introduction-1",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "refs/#",
+    "page": "References",
+    "title": "References",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "refs/#References-1",
+    "page": "References",
+    "title": "References",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "refs/#AAFreq-1",
+    "page": "References",
+    "title": "AAFreq",
+    "category": "section",
+    "text": "J. L. King, T. H. Jukes. Non-Darwinian Evolution. Science, 164(3881):788-98, 1969. doi:10.1126/science.164.3881.788"
+},
+
+{
+    "location": "refs/#ArStd-1",
+    "page": "References",
+    "title": "ArStd",
+    "category": "section",
+    "text": "J. Meija, T. B. Coplen, M. Berglund et al. Atomic weights of the elements 2013 (IUPAC Technical Report). Pure and Applied Chemistry, 88(3):265-91, 2016. doi:10.1515/pac-2015-0305"
+},
+
+{
+    "location": "refs/#RCov-1",
+    "page": "References",
+    "title": "RCov",
+    "category": "section",
+    "text": "B. Cordero, V. Gómez, A. E. Platero-Prats et al. Covalent radii revisited. Dalton Transactions, (21):2832-8, 2008. doi:10.1039/b801115j"
+},
+
+{
+    "location": "refs/#SimPW-1",
+    "page": "References",
+    "title": "SimPW",
+    "category": "section",
+    "text": "H. J. C. Berendsen. Simulating the Physical World, Cambridge University Press, 2007. ISBN-13: 978-0521835275"
+},
+
+{
+    "location": "license/#",
+    "page": "Software license",
+    "title": "Software license",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "license/#Software-license-1",
+    "page": "Software license",
+    "title": "Software license",
+    "category": "section",
+    "text": "The Dorothy.jl package can be used under the terms of the MIT “Expat” License:Copyright (c) 2019: Olivier Fisette.Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
+},
+
+]}
